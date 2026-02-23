87147460e3beb4aa2b35ecb9937ea8758fd5fafd
tegartamburaka9-svg <tegartamburaka9@gmail.com>
2026-02-23 21:27:04 +0700

    deklarasi2

---
+++ pertemuan2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+  
+int main(){//mulai
+    int p, l, luas;
+
+//langkah 2
+cout << "masukan Panjang : ";
+cin >>p;
+
+//langakh 3
+cout << "Masukan Lebar : ";
+cin >> 1;
+
+//langkah 4
+luas = p * l;
+}
